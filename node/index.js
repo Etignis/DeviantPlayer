6e0e671ef2c02954e3a24d6116de47a64cbfd0e7
Etignis <etignis@gmail.com>
2017-09-22 12:16:50 +0300

    feat[code]: music extension added

--- node/index.js
+++ node/index.js
@@ -4,30 +4,31 @@ const fs = require('fs');
 const path = require('path');
 
 const mp3Ext = '.mp3';
+const wavExt = '.wav';
 const sMusicPath = "D:/Cloud/DnD/Музыка";
 const sDBpath = "../js/db.js"
 
 function createBD() {
   let db = {}; // object exist
   console.log("Start working in " + sMusicPath);
-  fs.readdirSync(sMusicPath).forEach(way => {
-    const sInnerPath = path.join(sMusicPath, way);
+  fs.readdirSync(sMusicPath).forEach(folder => {
+    const sInnerPath = path.join(sMusicPath, folder);
     fs.lstat(sInnerPath, (err, stats) => {
       if(err)
         return console.log(err); //Handle error
       
       // only for folders with music
-      if(stats.isDirectory() && !(/^!/.test(way))) {
+      if(stats.isDirectory() && !(/^!/.test(folder))) {
         console.log(sInnerPath);
         let aList= [];
         fs.readdirSync(sInnerPath).forEach(file => {
           // music?
-          if(path.extname(file) === mp3Ext) {
+          if(path.extname(file) === mp3Ext || path.extname(file) === wavExt) {
             aList.push(file);
           }
         });
         // aList contain right data
-        db[way] = aList;
+        db[folder] = aList;
       }
     });
   });
