cc7043964e7b8345e95c5ba7c8dae626654c0ace
Etignis <etignis@gmail.com>
2018-07-06 12:41:06 +0300

    fix[bat] :create db

--- _create_music_DB.bat
+++ _create_music_DB.bat
@@ -1,8 +1,10 @@
 @echo off
 chcp 65001 
-setlocal enabledelayedexpansion
+setlocal DisableDelayedExpansion
 for /f "tokens=* delims=" %%a in ('dir /s/b') do (
   set "fulldir=%%~a"
+	setlocal EnableDelayedExpansion
   set "fulldir=!fulldir:\=\\!"
   >>DB.txt echo.!fulldir!
+	endlocal
 )
